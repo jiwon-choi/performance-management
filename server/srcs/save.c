5b086a94a731d033c31b8ced47d9940f3df56f9e
Jiwon-Choi <jwchoi.py@gmail.com>
2022-09-15 14:01:33 +0900

    :sparkles: 파일 저장 시 서버 측 저장 시간 추가

--- server/srcs/save.c
+++ server/srcs/save.c
@@ -1,6 +1,7 @@
 #include "../../agent/incs/packet.h"
 #include <stdio.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <unistd.h>
 
 void save_stat(struct s_packet* packet) {
@@ -13,7 +14,12 @@ void save_stat(struct s_packet* packet) {
   struct s_header* header = packet->data;
   struct s_stat* body = packet->data + sizeof(struct s_header);
 
-  fprintf(fp, "%s | %-8s | ", header->time, header->agent_name);
+  time_t raw_time = time(&raw_time);
+  char save_time[25];
+  strncpy(save_time, ctime(&raw_time), 25);
+  save_time[24] = 0;
+
+  fprintf(fp, "%s | %s | %-8s | ", header->time, save_time, header->agent_name);
   fprintf(fp, "user %-10d | sys %-10d | idle %-10d | iowait %-10d",
           body->user, body->sys, body->idle, body->iowait);
   fprintf(fp, "\n");
@@ -30,7 +36,12 @@ void save_mem(struct s_packet* packet) {
   struct s_header* header = packet->data;
   struct s_mem* body = packet->data + sizeof(struct s_header);
 
-  fprintf(fp, "%s | %-8s | ", header->time, header->agent_name);
+  time_t raw_time = time(&raw_time);
+  char save_time[25];
+  strncpy(save_time, ctime(&raw_time), 25);
+  save_time[24] = 0;
+
+  fprintf(fp, "%s | %s | %-8s | ", header->time, save_time, header->agent_name);
   fprintf(fp, "memtotal %-10d | memfree %-10d | swaptotal %-10d | swapfree %-10d",
           body->mem_total, body->mem_free, body->swap_total, body->swap_free);
   fprintf(fp, "\n");
@@ -46,9 +57,14 @@ void save_net(struct s_packet* packet) {
 
   struct s_header* header = packet->data;
 
+  time_t raw_time = time(&raw_time);
+  char save_time[25];
+  strncpy(save_time, ctime(&raw_time), 25);
+  save_time[24] = 0;
+
   for (int idx = 0; idx < header->number_of_body; idx++) {
     struct s_net* chunk = packet->data + sizeof(struct s_header) + sizeof(struct s_net) * idx;
-    fprintf(fp, "%s | %-8s | ", header->time, header->agent_name);
+    fprintf(fp, "%s | %s | %-8s | ", header->time, save_time, header->agent_name);
     fprintf(fp, "interface %-15s | Rbytes %-10d | Rpackets %-10d | Tbytes %-10d | Tpackets %-10d",
             chunk->interface, chunk->receive_bytes, chunk->receive_packets, chunk->transmit_bytes, chunk->transmit_packets);
     fprintf(fp, "\n");
@@ -64,9 +80,14 @@ void save_process(struct s_packet* packet) {
   }
   struct s_header* header = packet->data;
 
+  time_t raw_time = time(&raw_time);
+  char save_time[25];
+  strncpy(save_time, ctime(&raw_time), 25);
+  save_time[24] = 0;
+
   for (int idx = 0; idx < header->number_of_body; idx++) {
     struct s_process* chunk = packet->data + sizeof(struct s_header) + sizeof(struct s_process) * idx;
-    fprintf(fp, "%s | %-8s | ", header->time, header->agent_name);
+    fprintf(fp, "%s | %s | %-8s | ", header->time, save_time, header->agent_name);
     fprintf(fp, "pid %-6u | ppid %-6u | loginuid %-6u | ", chunk->pid, chunk->ppid, chunk->loginuid);
     // fprintf(fp, "cutime %10ld | cstime %10ld | utime %10lu | stime %10lu\n", chunk->cutime, chunk->cstime, chunk->utime, chunk->stime);
     fprintf(fp, "username %-8s | comm %-10s | cmdline : %s", chunk->username, chunk->comm, chunk->cmdline);
